efbe5ad844e5d71c4e4ad1c60789dfc5445d1cd1
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-09-12 00:26:41 -0400

    Updated the copy music/hpkvd scripts

--- copy_hpkvd.sh
+++ copy_hpkvd.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+PWD=$(pwd)
+HPDIR="../interface_hpkvd_run1"
+
+mkdir $HPDIR; cd $HPDIR
+
 rm -rf fields output param hpkvd-based-out.txt
 
 cp -r ../../fields ./
 cp -r ../../output ./
 cp -r ../../pp-music-interface/param ./
 cp ../../pp-music-interface/hpkvd-based-out.txt ./
+
+cd $PWD
--- copy_music.sh
+++ copy_music.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+PWD=$(pwd)
+HPDIR="../interface_music_run1"
+
+mkdir $HPDIR; cd $HPDIR
+
 rm -rf fields output param music-based-out.txt
 
 cp -r ../../fields ./
 cp -r ../../output ./
 cp -r ../../pp-music-interface/param ./
 cp ../../pp-music-interface/music-based-out.txt ./
+
+cd $PWD
