ddc33cbcd11ba15eb669d889e7f97ddbce812032
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-09-12 00:23:51 -0400

    Added scripts to copy the runs into hpkvd/music-specific dir

---
+++ copy_hpkvd.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm -rf fields output param hpkvd-based-out.txt
+
+cp -r ../../fields ./
+cp -r ../../output ./
+cp -r ../../pp-music-interface/param ./
+cp ../../pp-music-interface/hpkvd-based-out.txt ./
